ee853fd03ce1444a7c64918eeb632f40232bf8b2
niraj-jha <jhaniraj45@gmail.com>
2025-01-29 01:24:17 +0530

    Made some changes for production environment

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-providers";
 
+export const runtime = "edge";
+
 const spaceGrotesk = Space_Grotesk({
 	subsets: ['latin'],
 	weight: ['300', '400', '500', '600', '700'],
--- utils/mail.ts
+++ utils/mail.ts
@@ -1,4 +1,3 @@
-// services/emailService.ts
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
