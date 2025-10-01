5e45c990b83d9a5a84af897def3949f2633760e7
jha-niraj <jhaniraj45@gmail.com>
2025-10-01 16:30:50 +0530

    Updated the entry layout file for the better seo optimizations

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,22 @@ export default function RootLayout({
 		<html lang="en" suppressHydrationWarning>
 			<head>
 				<link rel="icon" href="/projectcentral.ico" />
+      
+                <title>Project Central</title>
+                <meta name="description" content="Streamline Your Projects with Ease" />
+
+                <meta property="og:url" content="https://projectcentral.nirajjha.xyz" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Project Central" />
+                <meta property="og:description" content="Streamline Your Projects with Ease" />
+                <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/e55831e0-2d0c-4c4e-8714-2bb667dc4170.png?token=IaDSxrXi48MuUZvj9Xp9Jk1xHZUJJpPKWwFYRxLzYJI&height=1024&width=1024&expires=33295316213" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="projectcentral.nirajjha.xyz" />
+                <meta property="twitter:url" content="https://projectcentral.nirajjha.xyz" />
+                <meta name="twitter:title" content="Project Central" />
+                <meta name="twitter:description" content="Streamline Your Projects with Ease" />
+                <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/e55831e0-2d0c-4c4e-8714-2bb667dc4170.png?token=IaDSxrXi48MuUZvj9Xp9Jk1xHZUJJpPKWwFYRxLzYJI&height=1024&width=1024&expires=33295316213" />
 			</head>
 			<body className={`${spaceGrotesk.className} ${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<Providers>
